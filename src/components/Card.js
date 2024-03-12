53fa3fd4c83bf6f1dace838912110b7e88990709
Anish <kirodiwlanish11@gmail.com>
2024-03-12 10:09:51 +0530

    Added Modal Feature and update UI

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
 import { AllItems } from '../Data'
+import Modal from "react-modal";
+import './Modal.css';
 import { AiFillFolderOpen, AiFillLike, AiOutlineLike, AiTwotoneEye } from 'react-icons/ai'
 
 export default function Card({ items }) {
     // console.log("searchTerm", searchTerm + " ");
     const [likedItems, setLikedItems] = useState([]);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const toggleModal = (image) => {
+        setSelectedImage(image);
+        setModalIsOpen(!modalIsOpen);
+    };
+
+    const closeModal = () => {
+        setSelectedImage(null);
+        setModalIsOpen(false);
+    };
 
     const handleLike = (index) => {
         // Find the corresponding item in the 'items' array
@@ -24,7 +38,7 @@ export default function Card({ items }) {
 
     return (
         <>
-            <div className="container-fluid px-6">
+            <div className="container-fluid px-6 image-view-modal-open">
 
                 <div className="grid md:grid-cols-3 lg:grid-cols-4  sm:grid-cols-2 gap-5 py-5">
                     {
@@ -33,7 +47,11 @@ export default function Card({ items }) {
                             return (
                                 <div key={index} className="category-item cursor-pointer">
                                     <div className="cat-img relative overflow-hidden rounded-md">
-                                        <img src={item.featureImg} alt={item.feUser} />
+                                        <img
+                                            src={item.featureImg}
+                                            alt={item.feUser}
+                                            onClick={() => toggleModal(item.featureImg)}
+                                        />
                                         <div className="save-file flex items-center absolute cursor-pointer top-2 left-2 py-2 px-3 bg-black/50 rounded-full">
                                             <div className="s-icon text-white text-sm pr-1">
                                                 <AiFillFolderOpen />
@@ -71,6 +89,17 @@ export default function Card({ items }) {
                 </div>
 
             </div>
+            {modalIsOpen && (
+                <div className="modal">
+                    <div onClick={closeModal} className="overlay"></div>
+                    <div className="modal-content">
+                        <img src={selectedImage} alt="Selected Image"></img>
+                        <button className="close-modal" onClick={closeModal}>
+                            CLOSE
+                        </button>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
